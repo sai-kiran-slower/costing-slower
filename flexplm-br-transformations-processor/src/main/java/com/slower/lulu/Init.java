d59f6d1756ca11973f6b4b8fa45bb8e1d35a6213
saikiran <sai@slower.ai>
2018-06-29 03:39:50 -0700

    IF2-Color transformations code

--- flexplm-br-transformations-processor/src/main/java/com/slower/lulu/Init.java
+++ flexplm-br-transformations-processor/src/main/java/com/slower/lulu/Init.java
@@ -9,15 +9,16 @@ public class Init {
     public static void main(String[] args) {
         try {
             System.out.println("Processing File: Style_1_FlexPLM.json");
-            File file = new File("src/main/resources/sample/Style_1_FlexPLM.json");
+            File file = new File("src/main/resources/sample/style/Style_1_FlexPLM.json");
             String absolutePath = file.getAbsolutePath();
             String file_content = new String(Files.readAllBytes(Paths.get(absolutePath)), StandardCharsets.UTF_8);
 
             PayloadProcessor payloadProcessor = new PayloadProcessor();
             String br_json_str = payloadProcessor.processStyle(file_content);
             System.out.println("Writing processed JSON into file: Style_1_BR_New.json");
-            Files.write(Paths.get("src/main/resources/sample/Style_1_BR_New.json"), br_json_str.getBytes());
+            Files.write(Paths.get("src/main/resources/sample/style/Style_1_BR_New.json"), br_json_str.getBytes());
 
+            System.out.println("");
 
             System.out.println("Processing File: Color_1_FlexPLM.json");
             file = new File("src/main/resources/sample/color/Color_1_FlexPLM.json");
--- flexplm-br-transformations-processor/src/main/java/com/slower/lulu/StyleResponseHandler.java
+++ flexplm-br-transformations-processor/src/main/java/com/slower/lulu/StyleResponseHandler.java
@@ -1,4 +1,130 @@
 package com.slower.lulu;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.*;
+
+import java.util.List;
+
 public class StyleResponseHandler {
+    public String handleStyle(StyleFlexPLMResponse styleFlexPLMResponse) throws Exception {
+        StyleBambooRoseResponse styleBambooRoseResponse = new StyleBambooRoseResponse();
+        StyleDocument styleDocument = new StyleDocument();
+        Request request = new Request();
+
+        QuoteBuilder quoteBuilder = new QuoteBuilder();
+        SizeRangeBuilder sizeRangeBuilder = new SizeRangeBuilder();
+        QuoteExtBuilder quoteExtBuilder = new QuoteExtBuilder();
+        AttachmentBuilder attachmentBuilder = new AttachmentBuilder();
+
+        List<Attribute> attributeList = styleFlexPLMResponse.getFlexInterface().getFlexPLMHeader().getAttributeList();
+
+        quoteBuilder.setOwner("LULULEMON");
+        quoteBuilder.setRequestType("FG");
+
+        for (Attribute attribute : attributeList) {
+            String field_name_key = attribute.getFIELDNAMEKEY();
+            if (attribute.getFIELDNAMEKEY().equalsIgnoreCase("stylenum")) {
+                quoteBuilder.setItemNo(attribute.getFIELDVALUEKEY());
+            }
+            else if (attribute.getFIELDNAMEKEY().equalsIgnoreCase("seasoncode")) {
+                quoteBuilder.setSeason(attribute.getFIELDVALUEKEY());
+            }
+            else if (attribute.getCHANGEIND().equalsIgnoreCase("Y")) {
+                switch (field_name_key.toLowerCase()) {
+                    case "stylenum":
+                        quoteBuilder.setItemNo(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "productname":
+                        quoteBuilder.setDescription(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "nameline1":
+                        quoteBuilder.setAltDesc1(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "productline":
+                        quoteBuilder.setCommodity(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "seasonalstyle":
+                        quoteBuilder.setStatus04(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "seasoncode":
+                        quoteBuilder.setSeason(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "year":
+                        quoteBuilder.setSeasonYear(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "masterstyleidnew":
+                        quoteBuilder.setBuyProgramNo(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "brand":
+                        quoteBuilder.setBrand(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "class":
+                        quoteBuilder.setPropertyClass(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "subclass":
+                        quoteBuilder.setSubclass(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "division":
+                        quoteBuilder.setDivision(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "category":
+                        quoteBuilder.setDept(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "status":
+                        quoteBuilder.setStatus(attribute.getFIELDVALUEKEY());
+                        break;
+                    case "sourcingmanager":
+                        quoteExtBuilder.setPatternMaker(attribute.getFIELDVALUEKEY());
+                        quoteBuilder.setQuoteExt(quoteExtBuilder.createQuoteExt());
+                        break;
+                    case "productdeveloper":
+                        quoteExtBuilder.setProdmgr(attribute.getFIELDVALUEKEY());
+                        quoteBuilder.setQuoteExt(quoteExtBuilder.createQuoteExt());
+                        break;
+                    case "designernew":
+                        quoteExtBuilder.setDesigner(attribute.getFIELDVALUEKEY());
+                        quoteBuilder.setQuoteExt(quoteExtBuilder.createQuoteExt());
+                        break;
+                    case "sizescale":
+                        sizeRangeBuilder.setSizeRange(attribute.getFIELDVALUEKEY());
+                        quoteBuilder.setSizeRanges(sizeRangeBuilder.createSizeRange());
+                        break;
+                    case "thumbnail":
+                        String img_loc = attribute.getFIELDVALUEKEY();
+                        String img_file_name = img_loc.substring(img_loc.lastIndexOf('/') + 1);
+                        attachmentBuilder.setLocation(img_file_name);
+                        attachmentBuilder.setAttachmentNo("IMAGE");
+                        attachmentBuilder.setCustomLocation("custom/" + img_file_name);
+                        attachmentBuilder.setIconLocation("icons/" + img_file_name);
+                        attachmentBuilder.setLargeviewLocation("largeview/" + img_file_name);
+                        attachmentBuilder.setOverviewLocation("overview/" + img_file_name);
+                        attachmentBuilder.setThumbnailLocation("thumbnails/" + img_file_name);
+                        quoteBuilder.setAttachment(attachmentBuilder.createAttachment());
+                        break;
+                    default:
+                        System.out.println("ERROR: Mapping not found for Attribute Key: "
+                                + attribute.getFIELDNAMEKEY()
+                                + ", Attribute Value: "
+                                + attribute.getFIELDVALUEKEY()
+                        );
+                }
+            }
+            else {
+                System.out.println("INFO: Change indicator is 'N' for Attribute Key: "
+                        + attribute.getFIELDNAMEKEY()
+                        + ", Attribute Value: "
+                        + attribute.getFIELDVALUEKEY()
+                );
+            }
+        }
+
+        request.setQuote(quoteBuilder.createQuote());
+        styleDocument.setRequest(request);
+        styleBambooRoseResponse.setDocument(styleDocument);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(styleBambooRoseResponse);
+    }
 }
